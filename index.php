e69261ae4f4d7055bd3703972947466951e2553b
alexandre <alexandre@localhost.localdomain>
2016-12-11 09:20:56 -0200

    Commit iniciando o projeto

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+require 'checkLogin.php';// importa o arquivo checkLogin e direciona para a tela de login
+require 'config.php';
+
+$query = "SELECT *FROM usuarios";
+$result = mysql_query($query);
+$registos = mysql_fetch_assoc($result);
+$usuario = isset($_SESSION['usuario']) ? $_SESSION['usuario'] : '';
+date_default_timezone_set('America/Sao_Paulo');
+$date = date('d-m-Y H:i');
+
+
+//echo 'Você está conectado na oficina AMX como usuário:  '.$usuario .' a data de hoje é ' .$date .' |<a href="logout.php">sair</a>';
+?>
+
+
+
+<!DOCTYPE html>
+<html >
+<head>
+  <title>Base conhecimento Zanthus</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/index.css" >
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+ </head>
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">Logo</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Menu</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Sair</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+  
+<div class="container-fluid text-center">    
+  <div class="row content">
+    <div class="col-sm-2 sidenav">
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+      <p><a href="#">Link</a></p>
+    </div>
+    <div class="col-sm-8 text-left"> 
+      <h1>Bem vindo</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <hr>
+      <h3>Test</h3>
+      <p>Lorem ipsum...</p>
+    </div>
+    <div class="col-sm-2 sidenav">
+      <div class="well">
+        <p>TESTE</p>
+      </div>
+      <div class="well">
+        <p>TESTE</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="container-fluid text-center">
+  <p>Texto de rodapé</p>
+</footer>
+
+</body>
+</html>
+
---
+++ login.php
@@ -0,0 +1,74 @@
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Base conhecimento Sitef</title>
+        <link rel="stylesheet" href="css/login.css" >
+    </head>
+    <body>
+        
+
+        <div class="form-wrapper">
+  
+          <form action="" method="post">
+            <h3>Acesse sua conta</h3>
+
+            <div class="form-item">
+                <input type="text" name="usuario" required="required" placeholder="Usuário" autofocus>
+            </div>
+
+            <div class="form-item">
+                <input type="password" name="senha" required="required" placeholder="Senha">
+            </div>
+
+            <div class="button-panel">
+                <input type="submit" class="button" title="Log In" value="Entrar">
+            </div>
+
+          </form>
+          <div class="reminder">
+            <p>Não é um membro? <a href="#">Cadastre-se</a></p>
+            <p><a href="#">Esqueceu a senha?</a></p>
+          </div>
+
+        </div>
+        
+	<?php
+            require 'config.php';
+            
+            if($_POST){
+                $usuario = $_POST['usuario'];//Cria variave usuário e senha para consultar no banco
+                $senha = $_POST['senha'];
+                
+                $query = "SELECT * FROM usuarios WHERE usuario = '$usuario' and senha = '$senha' "; //busca o usuário no banco
+                
+                $result = mysql_query($query);
+                $usuario = mysql_fetch_assoc($result);
+                
+                if($usuario){
+                    session_start();//inicia a sessão
+                    $_SESSION['logado'] = true;
+                    $_SESSION['usuario'] = $usuario['nome'];
+                    header('location:index.php');
+                }  else {
+                    echo '<h1>vc é um lixo</h1>';
+                    
+                }
+            }
+            
+            ?>
+        
+        
+        
+        
+        
+        
+        
+        
+    </body>
+</html>
